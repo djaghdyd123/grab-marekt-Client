7a914ff5150f5b56aa945a35d99f12a0e0d47de7
엄호룡 <eomholyong@eomholyong-ui-MacBookPro.local>
2021-03-23 15:56:50 +0900

    pages added

---
+++ src/product/index.js
@@ -0,0 +1,5 @@
+function ProductPage() {
+  return <h3>상품상세페이지</h3>;
+}
+
+export default ProductPage;
---
+++ src/upload/index.js
@@ -0,0 +1,5 @@
+function UploadPage() {
+  return <h3>업로드 화면</h3>;
+}
+
+export default UploadPage;
